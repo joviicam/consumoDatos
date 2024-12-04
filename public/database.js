1734e00498728e0512f5d81782824db6e509e059
Joviicam Racoon <105244358+joviicam@users.noreply.github.com>
2024-12-04 09:58:27 -0600

    Update database.js

--- public/database.js
+++ public/database.js
@@ -3,8 +3,8 @@ const db = new sqlite3.Database(':memory:');
 
 db.serialize(() => {
   db.run("CREATE TABLE user (id INT, name TEXT)");
-  db.run("INSERT INTO user (id, name) VALUES (1, 'John Doe')");
-  db.run("INSERT INTO user (id, name) VALUES (2, 'Jane Smith')");
+  db.run("INSERT INTO user (id, name) VALUES (1, 'Johana Alvarez')");
+  db.run("INSERT INTO user (id, name) VALUES (2, 'Checo Perez')");
 });
 
 module.exports = db;
